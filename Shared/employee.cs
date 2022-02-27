f919914ac92bd7ce5b24f70b3b622ec3d4005452
devyansh bhati <devyansh.bhati333@gmail.com>
2022-02-28 05:08:50 +0530

    first commitd

---
+++ Shared/employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_app_trial.Shared
+{
+    class employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName{ get; set; }
+        public string LastName{ get; set; }
+        public string Email{ get; set; }
+        public DateTime DateOfBirth{ get; set; }
+        public Gender Gender{ get; set; }
+        public int DepartmentId{ get; set; }
+        public String Photopath{ get; set; }
+        public Department Department{ get; set; }
+    }
+}
